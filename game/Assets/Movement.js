8bc3912bf7f2559c9b1010df00ea39942f777711
Nelvin Driz <nelvindriz@outlook.com>
2012-11-30 03:08:16 +0800

    Add Basic Gameplay
    Signed-off-by: Nelvin Driz <nelvindriz@live.com>

---
+++ game/Assets/Movement.js
@@ -0,0 +1,44 @@
+#pragma strict
+
+public var Boundary : int = 50;
+public var speed : int = 5;
+
+private var theScreenWidth : int;
+private var theScreenHeight : int;
+
+function Start() 
+{
+    theScreenWidth = Screen.width;
+    theScreenHeight = Screen.height;
+}
+
+function Update() 
+{
+    if (Input.GetKey("d") || Input.GetKey("right"))
+    {
+       transform.position.x += speed * Time.deltaTime;
+    }
+
+    if (Input.GetKey("a") || Input.GetKey("left"))
+    {
+       transform.position.x -= speed * Time.deltaTime;
+    }
+
+    if (Input.GetKey("w") || Input.GetKey("up"))
+    {
+       transform.position.y += speed * Time.deltaTime;
+    }
+
+    if (Input.GetKey("s") || Input.GetKey("down"))
+    {
+       transform.position.y -= speed * Time.deltaTime;
+    }
+
+}   
+
+function OnGUI() 
+{
+    // GUI.Box( Rect( (Screen.width / 2) - 140, 5, 280, 25 ), "Mouse Position = " + Input.mousePosition );
+    // GUI.Box( Rect( (Screen.width / 2) - 70, Screen.height - 30, 140, 25 ), "Mouse X = " + Input.mousePosition.x );
+    // GUI.Box( Rect( 5, (Screen.height / 2) - 12, 140, 25 ), "Mouse Y = " + Input.mousePosition.y );
+}
